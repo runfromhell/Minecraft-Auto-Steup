53bfb62dbe74a693f7801b3a4488bafadf80eb2a
Runfromhell <135614274+runfromhell@users.noreply.github.com>
2025-04-07 11:01:14 +0330

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# نصب جاوا
+sudo apt update
+sudo apt install -y openjdk-17-jre
+
+# ایجاد پوشه برای سرور ماینکرافت
+mkdir -p ~/minecraft_server
+cd ~/minecraft_server
+
+# دانلود فایل سرور ماینکرافت
+wget -O server.jar https://launcher.mojang.com/v1/objects/YOUR_SERVER_JAR_LINK.jar
+
+# اجرای سرور ماینکرافت
+echo "eula=true" > eula.txt
+java -Xmx1024M -Xms1024M -jar server.jar nogui
+chmod +x setup.sh
